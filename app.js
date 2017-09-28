ec15b04fdf32f5900cb8f00b2166ad8141ed05c5
Muhammad Usman <muhammad.usman@expertflow.com>
2017-09-28 19:47:07 +0500

    Working on development script

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-var nodegit = require('nodegit'),
-    path = require('path');
-
+const exec = require('child_process').exec;
+var _progress = require('cli-progress');
+var gitClone = "git clone";
 var url = "https://muhammad_usman_rana:hackman123@bitbucket.org/expertflow-ondemand/ccadmin.git";
-  var  local = "./baseApp";
-  var userName = "muhammad.usman@expertflow.com";
-  var password = "hackman123!"
-    var cloneOpts = {
-
-  };
-console.log("Cloning..");
-nodegit.Clone(url, local, cloneOpts).then(function (repo) {
-    console.log("Cloned " + path.basename(url) + " to " + repo.workdir());
-}).catch(function (err) {
-    console.log(err);
+var timer  ;
+   exec(gitClone +' '+url +'&& cd ccadmin && npm install && bower install', (a,b,c)=>{
+     clearInterval(timer);
+     b1.update(200)
+        b1.stop();
+   });
+   var b1 = new _progress.Bar({
+    barCompleteChar: '=',
+    barIncompleteChar: '.',
+    fps: 5,
+    stream: process.stdout,
+    barsize: 60
 });
+
+    b1.start(200, 0);
+    var value = 0;
+     timer = setInterval(function(){
+        value++;
+        b1.update(value)
+        if(value > 200){
+          value =0;
+        }
+    }, 20);
