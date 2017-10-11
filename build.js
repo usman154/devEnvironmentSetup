991e1e9bcc29bf7e6ad928b96869bad4a8e23560
Muhammad Usman <muhammad.usman@expertflow.com>
2017-10-11 15:08:32 +0500

    Error handling in build.js

--- build.js
+++ build.js
@@ -1,13 +1,35 @@
 const exec = require('child_process').exec;
+var _progress = require('cli-progress');
 const fsE = require('fs-extra');
 fsE.remove('newMicroService/src/app/baseApp', err => {
                      if (err) return console.error(err)
                       else console.log('\nDeleted base project!');
-				  
-				  var ex = exec('cd newMicroService && gulp serve:dist');		
-ex.on('close', function(code){
-	console.log(`command closed with exit code ${code}`)
-});
+				   pb1 = new progresBar();
+                   timer1 = new timer(pb1);
+				  var ex = exec('cd newMicroService && gulp serve:dist', (error, stdout, stderr)=>{
+					  console.log(`command closed with exit code ${code}  , error ${error} and stderr ${stderr}`);
+	                 pb1.update(100);
+                     pb1.stop();
+                    clearInterval(timer);
+				  });		
+ 
                     
                    });
+ function progresBar(){
+   var b1 = new _progress.Bar({format: 'Progress [{bar}] {percentage}% '}, _progress.Presets.shades_classic);
+    b1.start(100, 0);
+    return b1;
+}
+function timer(b1){
+  var value = 0;
+  var timer;
+   timer = setInterval(function(){
+      value++;
+      b1.update(value)
+      if(value > 100){
+        value =0;
+      }
+  }, 30);
+  return timer;
+}
 		   
